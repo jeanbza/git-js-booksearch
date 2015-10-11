baab24118d7d9c06eec8a6d422d3565df0295a68
Jean de Klerk <jadekler@gmail.com>
2015-10-11 12:57:30 -0600

    Simplify redux connector

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import App from './containers/app';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
 import configureStore from './store/configureStore';
+import Counter from './components/Counter'
+import * as CounterActions from './actions/counter'
 
 const store = configureStore();
 
+function mapStateToProps(state) {
+  return {
+    counter: state.counter
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(CounterActions, dispatch)
+}
+
+const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
 React.render(
   <Provider store={store}>
-  {() => <App />}
+  {() => <ConnectedApp />}
   </Provider>,
   document.getElementById('root')
 );
