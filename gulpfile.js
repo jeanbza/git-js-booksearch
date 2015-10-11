09233883d21fd2e15eb75d3871df8c3bfd3c3b44
Jean de Klerk <jadekler@gmail.com>
2015-10-10 18:44:38 -0600

    Added react.js and redux, with a simple test of a react.js component

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var del = require('del');
+
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+var paths = {
+  application_scripts: ['src/**/*.js'],
+  images: 'img/**/*'
+};
+
+gulp.task('images', ['clean:images'], function() {
+  return gulp.src(paths.images)
+    .pipe(gulp.dest('dist/img'));
+});
+
+/* JavaScript */
+gulp.task('browserify:js', ['clean:js'], function() {
+  var extensions = ['.js', '.json', '.es6'];
+  return browserify({
+      debug: true,
+      extensions: extensions
+    })
+    .transform(babelify.configure({
+      extensions: extensions
+    }))
+    .require("./src/app.js", {
+      entry: true
+    })
+    .bundle()
+    .pipe(source('app.min.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+/* Styles */
+gulp.task('scss', ['clean:css'], function() {
+  gulp.src('scss/**/*.scss')
+    .pipe(sass().on('error', function(err) {
+      console.error(err);
+    }))
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest('dist/css/'));
+});
+
+/* Clean up */
+gulp.task('clean:js', function() {
+  return del(['dist/js/*', 'dist/js']);
+});
+
+gulp.task('clean:css', function() {
+  return del(['dist/css/*', 'dist/css']);
+});
+
+gulp.task('clean:images', function() {
+  return del(['dist/img/*', 'dist/img']);
+});
+
+gulp.task('clean', ['clean:js', 'clean:css', 'clean:images']);
+
+gulp.task('default', ['clean', 'images', 'browserify:js', 'scss']);
---
+++ test/components/FooSpec.js
@@ -0,0 +1,11 @@
+import Foo from '../../src/components/Foo.jsx'
+import React from 'react/addons'
+
+const TestUtils = React.addons.TestUtils
+
+fdescribe('Foo component', () => {
+  it('renders with hello world', () => {
+    var fooComponent = TestUtils.renderIntoDocument( < Foo / > )
+    expect(React.findDOMNode(fooComponent).textContent).toEqual('Hello World')
+  })
+})
