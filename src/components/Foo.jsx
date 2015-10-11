7282b94a3ab994a43a9849ab1079d7b742e6ac02
Jean de Klerk <jadekler@gmail.com>
2015-10-11 12:14:38 -0600

    Removed foo chaff

--- src/components/Foo.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component, PropTypes} from 'react'
-
-export default class Foo extends Component {
-  render() {
-    return (
-      <div className="foo">Hello World</div>
-    )
-  }
-}
