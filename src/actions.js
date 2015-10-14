0801b345894afab398c1aa8273df5c0b8bf938d2
Jean de Klerk <jadekler@gmail.com>
2015-10-14 12:53:47 -0600

    Async request sent to google upon entering data into search box. Next up is displaying the resultant data below

--- src/actions.js
+++ src/actions.js
@@ -1,13 +1,42 @@
 export const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
 export const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
-export const SEARCH = 'SEARCH'
+export const SEARCH_STARTED = 'SEARCH_STARTED'
+export const SEARCH_SUCCEEDED = 'SEARCH_SUCCEEDED'
+export const SEARCH_FAILED = 'SEARCH_FAILED'
 
 export function search(searchBarEvent) {
   const searchBarData = searchBarEvent.target.value
 
-  return {
-    type: SEARCH,
-    searchBarData: searchBarData
+  return (dispatch) => {
+    dispatch({
+      type: SEARCH_STARTED,
+      searchBarData: searchBarData
+    })
+
+    return new Promise(function(resolve, reject) {
+      // TODO: the following can probably be a function
+
+      let req = new XMLHttpRequest()
+      req.open("GET", "https://www.googleapis.com/books/v1/volumes?q=" + searchBarData)
+      req.onload = function() {
+        if (req.status === 200) {
+          resolve(req.response)
+        } else {
+          reject(new Error(req.statusText))
+        }
+      }
+
+      req.onerror = function() {
+        reject(new Error("Network error"))
+      }
+
+      req.send()
+    })
+    .then(JSON.parse)
+    .then(
+      (result) => dispatch({ type: SEARCH_SUCCEEDED, searchResult: result }),
+      (error) =>  dispatch({ type: SEARCH_FAILED, searchError: error })
+    )
   }
 }
 
--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,12 +1,17 @@
-import { SEARCH } from '../actions'
+import { SEARCH_STARTED, SEARCH_SUCCEEDED, SEARCH_FAILED } from '../actions'
 
 export default function search(state = {}, action) {
   switch (action.type) {
-  case SEARCH:
-    state.searchBarData = action.searchBarData
+  case SEARCH_STARTED:
+    console.log("search started!")
+    return state
+  case SEARCH_SUCCEEDED:
+    console.log(action.searchResult)
 
-    // do something ajax-y here?
-
+    console.log("search finished!")
+    return state
+  case SEARCH_FAILED:
+    console.log("search failed!")
     return state
   default:
     return state
