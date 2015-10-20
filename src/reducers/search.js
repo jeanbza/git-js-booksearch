036140bc51555c78d0fa52f23a5cf8a75bc4ad1d
Jean de Klerk <jadekler@gmail.com>
2015-10-19 21:28:04 -0600

    Added search spec, removed counter spec

--- src/reducers/search.js
+++ src/reducers/search.js
@@ -4,13 +4,6 @@ export default function search(state = {}, action) {
   switch (action.type) {
   case SEARCH_STARTED:
     return state
-  default:
-    return state
-  }
-}
-
-export default function searchResults(state = {}, action) {
-  switch (action.type) {
   case SEARCH_SUCCEEDED:
     const books = action.searchResult.items.map(function(item) {
       return {
---
+++ test/reducers/SearchSpec.js
@@ -0,0 +1,31 @@
+import search from '../../src/reducers/search'
+import { SEARCH_STARTED, SEARCH_SUCCEEDED, SEARCH_FAILED } from '../../src/actions'
+
+describe('reducers', () => {
+  describe('search', () => {
+    it('should handle initial state', () => {
+      expect(search(undefined, {})).toEqual({});
+    })
+
+    it('should handle SEARCH_STARTED', () => {
+      expect(search({}, { type: SEARCH_STARTED })).toEqual({});
+    })
+
+    it('should handle SEARCH_SUCCEEDED', () => {
+      const fakeResult = {
+        items: []
+      }
+
+      const searchSuccessResult = search({}, {
+        type: SEARCH_SUCCEEDED,
+        searchResult: fakeResult
+      })
+
+      expect(searchSuccessResult).toEqual([])
+    })
+
+    it('should handle SEARCH_FAILED', () => {
+      expect(search({}, { type: SEARCH_FAILED })).toEqual({})
+    })
+  })
+})
